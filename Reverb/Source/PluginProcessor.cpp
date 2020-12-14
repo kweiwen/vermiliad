a9753d7ce61d930b18b5712daf421b315e5233a0
kweiwen <kweiwen.tseng@gmail.com>
2020-12-14 20:28:26 +0800

    first commit reverb

---
+++ Reverb/Source/PluginProcessor.cpp
@@ -0,0 +1,219 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+CircularBufferAudioProcessor::CircularBufferAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+                     #endif
+                       )
+#endif
+{
+    addParameter (gain        = new juce::AudioParameterFloat ("gain", "Gain", 0.0f, 1.0f, 0.5f));
+    addParameter (time        = new juce::AudioParameterFloat ("time", "Time", 0.0f, 1.0f, 0.5f));
+}
+
+CircularBufferAudioProcessor::~CircularBufferAudioProcessor()
+{
+}
+
+//==============================================================================
+const juce::String CircularBufferAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool CircularBufferAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool CircularBufferAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool CircularBufferAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double CircularBufferAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int CircularBufferAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int CircularBufferAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void CircularBufferAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const juce::String CircularBufferAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void CircularBufferAudioProcessor::changeProgramName (int index, const juce::String& newName)
+{
+}
+
+//==============================================================================
+void CircularBufferAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    // Use this method as the place to do any pre-playback
+    // initialisation that you need..
+    _mCircularBuffer = new CircularBuffer<float>[getTotalNumInputChannels()];
+    _mDelayParaSmooth = new ParamSmooth*[getTotalNumInputChannels()];
+    
+    for (int index = 0; index < getTotalNumInputChannels(); index++)
+    {
+        _mCircularBuffer[index].createCircularBuffer(sampleRate);
+        _mCircularBuffer[index].flushBuffer();
+        
+        _mDelayParaSmooth[index] = new ParamSmooth(sampleRate/100, sampleRate);
+    }
+}
+
+void CircularBufferAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+    delete [] _mCircularBuffer;
+    _mCircularBuffer = nullptr;
+    
+    for (int index = 0; index < getTotalNumInputChannels(); index++)
+    {
+        delete _mDelayParaSmooth[index];
+    }
+    delete [] _mDelayParaSmooth;
+    _mDelayParaSmooth = nullptr;
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool CircularBufferAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    juce::ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+void CircularBufferAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+{
+    juce::ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+    if (debug) DBG ("totalNumInputChannels: " << totalNumInputChannels);
+    if (debug) DBG ("totalNumOutputChannels: " << totalNumOutputChannels);
+
+
+    // In case we have more outputs than inputs, this code clears any output
+    // channels that didn't contain input data, (because these aren't
+    // guaranteed to be empty - they may contain garbage).
+    // This is here to avoid people getting screaming feedback
+    // when they first compile a plugin, but obviously you don't need to keep
+    // this code if your algorithm always overwrites all the output channels.
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+
+    // This is the place where you'd normally do the guts of your plugin's
+    // audio processing...
+    // Make sure to reset the state if your inner loop is processing
+    // the samples and the outer loop is handling the channels.
+    // Alternatively, you can process the samples with the channels
+    // interleaved by keeping the same state.
+    for (int channel = 0; channel < totalNumInputChannels; ++channel)
+    {
+        auto* channelData = buffer.getWritePointer (channel);
+        for (int sample = 0; sample < buffer.getNumSamples(); sample++)
+        {
+            // ..do something to the data...
+            _mCircularBuffer[channel].writeBuffer(channelData[sample]);
+            auto delaySample = _mDelayParaSmooth[channel]->process(time->get());
+            channelData[sample] = _mCircularBuffer[channel].readBuffer(delaySample * getSampleRate()) * gain->get();
+        }
+    }
+}
+
+//==============================================================================
+bool CircularBufferAudioProcessor::hasEditor() const
+{
+    return false; // (change this to false if you choose to not supply an editor)
+}
+
+juce::AudioProcessorEditor* CircularBufferAudioProcessor::createEditor()
+{
+    //return new NewProjectAudioProcessorEditor (*this);
+    return nullptr;
+}
+
+//==============================================================================
+void CircularBufferAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+}
+
+void CircularBufferAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new CircularBufferAudioProcessor();
+}
